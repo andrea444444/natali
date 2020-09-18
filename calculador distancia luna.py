2bd473aa3c32ee885374b0f458cbb2b1706ef673
natali <Andrwruedagutierrez00@gmail.com>
2020-09-18 17:04:32 -0400

    distancia recorrido

--- calculador distancia luna.py
+++ calculador distancia luna.py
@@ -1,4 +1,4 @@
 distancia_tierra_luna = 384408
-distancia_recorrida = 50000
+distancia_recorrida = int(input("por favor, ingrese distancia recorrida  :"))
 distancia_restante = distancia_tierra_luna - distancia_recorrida
-print(distancia_restante)
+print("distancia restante" , distancia_restante)
