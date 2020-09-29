1ae91b1f0681aa40be97869fb6f27a50372a62a7
natali <Andrwruedagutierrez00@gmail.com>
2020-09-29 17:19:14 -0400

    añadir ejercicio 5 de la clase

--- variables/ejercicio 5.py
+++ variables/ejercicio 5.py
@@ -1,5 +1,5 @@
-horas = input("¿Cuántas horas trabajó?")
-horas= int(horas)
-pago_x_hora = "50"
-pago_x_hora = int(pago_x_hora)
-pago_total = print(horas * pago_x_hora)
+horas = int(input("¿Cuántas horas trabajó?"))
+pago_x_hora = float(input("Ingrese paga por hora:"))
+pago_total = horas * pago_x_hora
+mensaje = "monto total a pagar" + str(pago_total)
+print(mensaje)
