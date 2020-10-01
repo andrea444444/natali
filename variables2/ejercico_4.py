cafd74fa1c3c9c5e751b00da525009cefac9a41a
natali <Andrwruedagutierrez00@gmail.com>
2020-10-01 17:10:16 -0400

    añadir ejercicio cuatro, clase 2

---
+++ variables2/ejercico_4.py
@@ -0,0 +1,10 @@
+num_1 = int(input("Ingrese primer número"))
+num_2 = int(input("Ingrese segundo número"))
+iguales = (num_1 == num_2)
+print(f"los números son iguales: {iguales}")
+distintos = num_1 != num_2
+print(f"los números son distintos: {distintos}")
+num_1_mayor = num_1 > num_2
+print(f"el número 1 es mayor al número 2: {num_1_mayor}")
+num_1_menor = num_1<=num_2
+print(f"El primer número es menor o igual al segundo número: {num_1_menor}")
