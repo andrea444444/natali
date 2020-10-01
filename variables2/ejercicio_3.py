52fa060c46b4152a26a4300f758bf3c39dd10d6a
natali <Andrwruedagutierrez00@gmail.com>
2020-10-01 17:15:57 -0400

    añadir ejercicio tres, clase 2

---
+++ variables2/ejercicio_3.py
@@ -0,0 +1,5 @@
+radio = (float(input("Ingrese el radio por favor:")))
+area =  3.14 *radio ** 2
+perimetro = 2 * 3.14 * radio
+f"El área del círculo es {area} y el perímetro es {perimetro}"
+print(f"El área del círculo es {area} y el perímetro es {perimetro}")
