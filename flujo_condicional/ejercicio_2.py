d84fb418db417e4cd736d20d467e58fd4de014a9
natali <Andrwruedagutierrez00@gmail.com>
2020-10-02 17:21:49 -0400

    añadir ejercicio 2, flujo condicional

---
+++ flujo_condicional/ejercicio_2.py
@@ -0,0 +1,8 @@
+num_1 = int(input("Ingrese primer número"))
+num_2 = int(input("Ingrese segundo número"))
+if num_1 > num_2:
+    print(num_1)
+elif num_1 < num_2:
+    print(num_2)
+else :
+    print("los números son iguales")
