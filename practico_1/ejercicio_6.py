9d4d32aba79d17ff54cd44d1bafec1437b34fa7b
natali <Andrwruedagutierrez00@gmail.com>
2020-10-09 16:23:41 -0400

    añadir ejercicio 6, practico 1

--- practico_1/ejercicio_6.py
+++ practico_1/ejercicio_6.py
@@ -1,6 +1,6 @@
 num_1 = int(input("Ingrese el primer número por favor: "))
 num_2 = int(input("Ingrese el segundo número por favor: "))
-if num_1<num_2:
+if num_1>num_2:
     print(num_2, num_1 )
-elif num_2<num_1:
+elif num_2>num_1:
     print(num_1, num_2)
