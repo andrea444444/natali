bd263e55efb19c33ce671b819f948d81303ea546
natali <Andrwruedagutierrez00@gmail.com>
2020-10-26 20:37:18 -0400

    ejercicio 1, practico 2 número mayor

---
+++ Funciones/EJERCICIO_1.py
@@ -0,0 +1,8 @@
+def numM(num1,num2):
+    if num1>num2:
+        print(num1)
+    elif num1 < num2:
+            print(num2)
+    else:
+        print("Son iguales")
+numM((int(input("Número 1: "))),(int(input("Número 2: "))))
