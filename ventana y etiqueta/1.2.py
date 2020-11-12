4137f8238f8cc37a192bcac3d13c9795662e9051
natali <Andrwruedagutierrez00@gmail.com>
2020-11-12 14:20:32 -0400

    suma, resta, división y multiplicación

---
+++ ventana y etiqueta/1.2.py
@@ -0,0 +1,75 @@
+import tkinter as tk
+from tkinter import *
+
+def limpiarCampos():
+    NA.set(0)
+    NB.set(0)
+
+def sumar():
+    numeroA=NA.get()
+    numeroB=NB.get()
+    numeroR=numeroA+numeroB
+    NR.set(numeroR)
+    limpiarCampos()
+
+def resta():
+    numeroA=NA.get()
+    numeroB=NB.get()
+    numeroR = numeroA - numeroB
+    NR.set(numeroR)
+    limpiarCampos()
+
+def multiplicacion():
+    numeroA=NA.get()
+    numeroB=NB.get()
+    numeroR = numeroA * numeroB
+    NR.set(numeroR)
+    limpiarCampos()
+
+def division():
+    numeroA=NA.get()
+    numeroB=NB.get()
+    numeroR = numeroA / numeroB
+    NR.set(numeroR)
+    limpiarCampos()
+
+ventana=tk.Tk()
+ventana.config(width=300,height=200)
+
+etiquetaNA=Label(ventana,text="Número A:")
+etiquetaNA.place(x=0,y=0)
+
+NA=IntVar()
+entradaNA=Entry(ventana,textvariable=NA)
+entradaNA.place(x=70,y=0)
+
+
+
+etiquetaNB=Label(ventana,text="Número B:")
+etiquetaNB.place(x=0,y=30)
+
+NB=IntVar()
+entradaNB=Entry(ventana,textvariable=NB)
+entradaNB.place(x=70,y=30)
+
+
+etiquetaRES=Label(ventana,text="Resultado:")
+etiquetaRES.place(x=0,y=60)
+
+NR=IntVar()
+salidaRES=Entry(ventana,textvariable=NR)
+salidaRES.place(x=70,y=60)
+
+botonTransportar=Button(ventana,text="Sumar A y B", command=sumar)
+botonTransportar.place(x=0,y=90)
+
+botonTransportar=Button(ventana,text="Restar A y B", command=resta)
+botonTransportar.place(x=0,y=120)
+
+botonTransportar=Button(ventana,text="Multiplicar A y B", command=multiplicacion)
+botonTransportar.place(x=0,y=150)
+
+botonTransportar=Button(ventana,text="Dividir A y B", command=division)
+botonTransportar.place(x=0,y=180)
+
+ventana.mainloop()
