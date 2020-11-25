3954fad1082b9c17d1e295bfd443e62e053b9dcb
natali <Andrwruedagutierrez00@gmail.com>
2020-11-25 12:11:41 -0400

    ejercicio 1

--- vectores/ejercicio_1.py
+++ vectores/ejercicio_1.py
@@ -1,12 +1,12 @@
+def FACT(vectorFact):
+    for n in vectorFact:
+        num = n
+        multiplicacion = 1
+        for i in range (1,n+1):
+            multiplicacion= multiplicacion * i
+        print(f"Factorial de {i}: {multiplicacion}")
 
 vectorFact=[]
 for i in range (1,21):
     vectorFact.append(i)
-
-for n in vectorFact:
-    num = n
-    multiplicacion = 1
-    for i in range (1,n+1):
-        multiplicacion= multiplicacion * i
-    print(f"Factorial de {i}: {multiplicacion}")
-
+FACT(vectorFact)
