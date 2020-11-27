fd9a5bcb1c3f1efb447f9e496843acafcf0d8488
natali <Andrwruedagutierrez00@gmail.com>
2020-11-27 16:42:56 -0400

    ejercicio 2

--- vectores/ejercicio_2.1.py
+++ vectores/ejercicio_2.1.py
@@ -6,7 +6,9 @@ for i in range(0,5):
 for i in range(0,5):
     num2 = int(input("Ingrese un número para el vector B: "))
     vectorB.append(num2)
-
+vectorSuma = []
 for i in range (0,5):
     suma = vectorA[i]+vectorB[i]
     print(f"El vector suma del componente {i+1} es : {suma}")
+    vectorSuma.append(suma)
+print(vectorSuma)
